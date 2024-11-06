c032dbe480ac25c3e330a840618341c44115e9de
NOBITA BOT DEVLOPER <155904878+vishalpandeynkp@users.noreply.github.com>
2024-11-07 02:37:48 +0530

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,10 +1,4 @@
-#
-# Copyright (C) 2024 by THE-VIP-BOY-OP@Github, < https://github.com/THE-VIP-BOY-OP >.
-#
-# This file is part of < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC/blob/master/LICENSE >
-#
+
 # All rights reserved
 
 import os
