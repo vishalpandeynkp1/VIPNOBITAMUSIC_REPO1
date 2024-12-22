357ea701e96e3edcd472341c8a9a75a98b33885e
NOBITA BOT DEVLOPER <155904878+vishalpandeynkp@users.noreply.github.com>
2024-12-22 20:10:16 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -108,15 +108,15 @@ AUTO_GCAST_MSG = getenv("AUTO_GCAST_MSG", "")
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/Lucky_jet_game_signal"
-)  # Example:- https://t.me/Lucky_jet_game_signal
+    "SUPPORT_CHANNEL", "https://t.me/NOBITA_ALL_BOT"
+)  # Example:- https://t.me/NOBITA_ALL_BOT
 SUPPORT_GROUP = getenv(
-    "BOYS_STATUS_GROUP", "https://t.me/lucky_jet_aviator_prediction"
-)  # Example:- https://t.me/lucky_jet_aviator_prediction
+    "BOYS_STATUS_GROUP", "https://t.me/CN_STATUS"
+)  # Example:- https://t.me/CN_STATUS
 
 SUPPORT_CHAT = getenv(
-    "SUPPORT_GROUP", "https://t.me/Betting_Fantasy_Tips"
-)  # Example:- https://t.me/TG_FRIENDSS
+    "SUPPORT_GROUP", "https://t.me/NOBITA_SUPPORT"
+)  # Example:- https://t.me/NOBITA_SUPPORT
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", False)
 
@@ -211,67 +211,67 @@ autoclean = []
 
 START_IMG_URL = getenv(
     "START_IMG_URL",
-    "https://envs.sh/ctj.jpg",
+    "https://files.catbox.moe/0fm2u1.jpg",
 )
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
-    "https://envs.sh/ctj.jpg",
+    "https://files.catbox.moe/0fm2u1.jpg",
 )
 
 PLAYLIST_IMG_URL = getenv(
     "PLAYLIST_IMG_URL",
-    "https://envs.sh/ctj.jpg",
+    "https://files.catbox.moe/0fm2u1.jpg",
 )
 
 GLOBAL_IMG_URL = getenv(
     "GLOBAL_IMG_URL",
-    "https://envs.sh/ctj.jpg",
+    "https://files.catbox.moe/0fm2u1.jpg",
 )
 
 STATS_IMG_URL = getenv(
     "STATS_IMG_URL",
-    "https://envs.sh/ctj.jpg",
+    "https://files.catbox.moe/0fm2u1.jpg",
 )
 
 TELEGRAM_AUDIO_URL = getenv(
     "TELEGRAM_AUDIO_URL",
-    "https://envs.sh/ctj.jpg",
+    "https://files.catbox.moe/0fm2u1.jpg",
 )
 
 TELEGRAM_VIDEO_URL = getenv(
     "TELEGRAM_VIDEO_URL",
-    "https://envs.sh/ctj.jpg",
+    "https://files.catbox.moe/0fm2u1.jpg",
 )
 
 STREAM_IMG_URL = getenv(
     "STREAM_IMG_URL",
-    "https://envs.sh/ctj.jpg",
+    "https://files.catbox.moe/0fm2u1.jpg",
 )
 
 SOUNCLOUD_IMG_URL = getenv(
     "SOUNCLOUD_IMG_URL",
-    "https://envs.sh/ctj.jpg",
+    "https://files.catbox.moe/0fm2u1.jpg",
 )
 
 YOUTUBE_IMG_URL = getenv(
     "YOUTUBE_IMG_URL",
-    "https://envs.sh/ctj.jpg",
+    "https://files.catbox.moe/0fm2u1.jpg",
 )
 
 SPOTIFY_ARTIST_IMG_URL = getenv(
     "SPOTIFY_ARTIST_IMG_URL",
-    "https://envs.sh/ctj.jpg",
+    "https://files.catbox.moe/0fm2u1.jpg",
 )
 
 SPOTIFY_ALBUM_IMG_URL = getenv(
     "SPOTIFY_ALBUM_IMG_URL",
-    "https://envs.sh/ctj.jpg",
+    "https://files.catbox.moe/0fm2u1.jpg",
 )
 
 SPOTIFY_PLAYLIST_IMG_URL = getenv(
     "SPOTIFY_PLAYLIST_IMG_URL",
-    "https://envs.sh/ctj.jpg",
+    "https://files.catbox.moe/0fm2u1.jpg",
 )
 
 
@@ -312,7 +312,7 @@ if GITHUB_REPO:
 
 
 if PING_IMG_URL:
-    if PING_IMG_URL != "https://envs.sh/ctj.jpg":
+    if PING_IMG_URL != "https://files.catbox.moe/0fm2u1.jpg":
         if not re.match("(?:http|https)://", PING_IMG_URL):
             print(
                 "[ERROR] - Your PING_IMG_URL url is wrong. Please ensure that it starts with https://"
@@ -320,7 +320,7 @@ if PING_IMG_URL:
 
 
 if PLAYLIST_IMG_URL:
-    if PLAYLIST_IMG_URL != "https://envs.sh/ctj.jpg":
+    if PLAYLIST_IMG_URL != "https://files.catbox.moe/0fm2u1.jpg":
         if not re.match("(?:http|https)://", PLAYLIST_IMG_URL):
             print(
                 "[ERROR] - Your PLAYLIST_IMG_URL url is wrong. Please ensure that it starts with https://"
@@ -328,7 +328,7 @@ if PLAYLIST_IMG_URL:
 
 
 if GLOBAL_IMG_URL:
-    if GLOBAL_IMG_URL != "https://envs.sh/ctj.jpg":
+    if GLOBAL_IMG_URL != "https://files.catbox.moe/0fm2u1.jpg":
         if not re.match("(?:http|https)://", GLOBAL_IMG_URL):
             print(
                 "[ERROR] - Your GLOBAL_IMG_URL url is wrong. Please ensure that it starts with https://"
@@ -336,7 +336,7 @@ if GLOBAL_IMG_URL:
 
 
 if STATS_IMG_URL:
-    if STATS_IMG_URL != "https://envs.sh/ctj.jpg":
+    if STATS_IMG_URL != "https://files.catbox.moe/0fm2u1.jpg":
         if not re.match("(?:http|https)://", STATS_IMG_URL):
             print(
                 "[ERROR] - Your STATS_IMG_URL url is wrong. Please ensure that it starts with https://"
@@ -344,7 +344,7 @@ if STATS_IMG_URL:
 
 
 if TELEGRAM_AUDIO_URL:
-    if TELEGRAM_AUDIO_URL != "https://envs.sh/ctj.jpg":
+    if TELEGRAM_AUDIO_URL != "https://files.catbox.moe/0fm2u1.jpg":
         if not re.match("(?:http|https)://", TELEGRAM_AUDIO_URL):
             print(
                 "[ERROR] - Your TELEGRAM_AUDIO_URL url is wrong. Please ensure that it starts with https://"
@@ -352,7 +352,7 @@ if TELEGRAM_AUDIO_URL:
 
 
 if STREAM_IMG_URL:
-    if STREAM_IMG_URL != "https://envs.sh/ctj.jpg":
+    if STREAM_IMG_URL != "https://files.catbox.moe/0fm2u1.jpg":
         if not re.match("(?:http|https)://", STREAM_IMG_URL):
             print(
                 "[ERROR] - Your STREAM_IMG_URL url is wrong. Please ensure that it starts with https://"
@@ -360,7 +360,7 @@ if STREAM_IMG_URL:
 
 
 if SOUNCLOUD_IMG_URL:
-    if SOUNCLOUD_IMG_URL != "https://envs.sh/ctj.jpg":
+    if SOUNCLOUD_IMG_URL != "https://files.catbox.moe/0fm2u1.jpg":
         if not re.match("(?:http|https)://", SOUNCLOUD_IMG_URL):
             print(
                 "[ERROR] - Your SOUNCLOUD_IMG_URL url is wrong. Please ensure that it starts with https://"
@@ -368,7 +368,7 @@ if SOUNCLOUD_IMG_URL:
 
 
 if YOUTUBE_IMG_URL:
-    if YOUTUBE_IMG_URL != "https://envs.sh/ctj.jpg":
+    if YOUTUBE_IMG_URL != "https://files.catbox.moe/0fm2u1.jpg":
         if not re.match("(?:http|https)://", YOUTUBE_IMG_URL):
             print(
                 "[ERROR] - Your YOUTUBE_IMG_URL url is wrong. Please ensure that it starts with https://"
@@ -376,7 +376,7 @@ if YOUTUBE_IMG_URL:
 
 
 if TELEGRAM_VIDEO_URL:
-    if TELEGRAM_VIDEO_URL != "https://envs.sh/ctj.jpg":
+    if TELEGRAM_VIDEO_URL != "https://files.catbox.moe/0fm2u1.jpg":
         if not re.match("(?:http|https)://", TELEGRAM_VIDEO_URL):
             print(
                 "[ERROR] - Your TELEGRAM_VIDEO_URL url is wrong. Please ensure that it starts with https://"
